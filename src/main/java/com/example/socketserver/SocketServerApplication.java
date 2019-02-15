e96578d9b03b9acf46b15e7735eac3758067a4b3
unknown <kevin.c.bai0@gmail.com>
2019-02-14 19:33:31 -0500

    dashboard

--- src/main/java/com/example/socketserver/SocketServerApplication.java
+++ src/main/java/com/example/socketserver/SocketServerApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocketServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SocketServerApplication.class, args);
 	}
-
 }
 
